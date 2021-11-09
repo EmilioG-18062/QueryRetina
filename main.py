e287eb250a7a7d6ebc8fe9e4f67e202c1296652f
EmilioG-18062 <gor18062@uvg.edu.gt>
2021-11-09 13:35:40 -0600

    Test Git

--- main.py
+++ main.py
@@ -34,3 +34,5 @@ if __name__ == '__main__':
 
     with open('outdated_data.json', 'w', encoding='utf-8') as f:
         json.dump(mysql_table, f, ensure_ascii=False, indent=4)
+
+    print("Hola mundo")
