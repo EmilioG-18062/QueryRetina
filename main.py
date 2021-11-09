159b438d5bc302645b683072e5f3c448610386b7
EmilioG-18062 <gor18062@uvg.edu.gt>
2021-11-09 13:32:00 -0600

    Finish connection, search and save data from MySQL data-base

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 # Retina
-#
+# MySQL query
 
-def print_hi(name):
-    print("Hola mumdo")
+import pymysql
+import json
+
+# variables to establish connection with MySQL
+mysql_host = "localhost"
+mysql_user = "user"
+mysql_passwd = "passwd"
+mysql_database = "database"
+
+# variables for delimited search within the DB
+column_d1 = "estado"
+
+
+def query_select():
+    # database connection
+    connection = pymysql.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, database=mysql_database)
+    cursor = connection.cursor()
+
+    retrieve = "SELECT * FROM {database} WHERE {column} = 0".format(column=column_d1, database=mysql_database)
+    cursor.execute(retrieve)
+    result = cursor.fetchall()
+
+    # committing the connection then closing it.
+    connection.commit()
+    connection.close()
+    return result
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    mysql_table = query_select()
 
-
-
+    with open('outdated_data.json', 'w', encoding='utf-8') as f:
+        json.dump(mysql_table, f, ensure_ascii=False, indent=4)
