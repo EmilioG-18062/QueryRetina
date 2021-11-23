60c2e32bae745436f4c857bc4db2c2981b0e878c
EmilioG-18062 <gor18062@uvg.edu.gt>
2021-11-22 18:05:12 -0600

    Clean some functions

--- query_select.py
+++ query_select.py
@@ -18,8 +18,8 @@ mysql_table2 = "table"
 # variables for delimited search within the DB
 column_d1 = "sincronizado"
 column_d2 = "fecha_hora"
-fecha_hora1 = ""
-fecha_hora2 = ""
+fecha_hora1 = "2021-11-01 00:02:06"
+fecha_hora2 = "2021-11-01 07:26:42"
 
 # variables to establish connection with SFTP
 myHostname = "yourserverdomainorip.com"
@@ -29,33 +29,40 @@ folder_path = r'D:\PycharmProjects\QueryRetina'
 file_type = '\*py'
 
 
-def reformatting_data(data: list) -> list:
+def query_select() -> list:
     # database connection
     connection = pymysql.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, database=mysql_db)
     cursor = connection.cursor()
 
+    retrieve = f"SELECT * FROM {mysql_table1} WHERE ({column_d2} BETWEEN '{fecha_hora1}' AND '{fecha_hora2}') AND {column_d1}=0"
+    cursor.execute(retrieve)
+    outdated_data = cursor.fetchall()
+
     retrieve = f"SELECT id, retina_sensores FROM {mysql_table2}"
     cursor.execute(retrieve)
-    result = cursor.fetchall()
+    sensor_id_list = cursor.fetchall()
 
     # committing the connection then closing it.
     connection.commit()
     connection.close()
 
+    # Converting tuples to list
+    data = [list(elem) for elem in outdated_data]
+
     # changing id and sincronizado value then saving in data
-    ref_dict = {str(var[0]): str(var[1]) for var in result}
-    for listv in data:
-        listv[1] = ref_dict[str(listv[1])]
-        listv[3] = 1
+    id_dictionary = {str(sensor_id[0]): str(sensor_id[1]) for sensor_id in sensor_id_list}
+    for block in data:
+        block[1] = id_dictionary[str(block[1])]
+        block[3] = 1
 
     # Sorting by sensor
     sensor = {}
     fechas = []
-    for var in data:
-        temp = sensor.get(var[1], [])
-        temp.append(var[2])
-        sensor[var[1]] = temp
-        fechas.append(str(var[0]))
+    for block in data:
+        temp = sensor.get(block[1], [])
+        temp.append(block[2])
+        sensor[block[1]] = temp
+        fechas.append(str(block[0]))
 
     # Getting the length of each sensor list
     lists_sizes = [len(value) for key, value in sensor.items()]
@@ -68,41 +75,23 @@ def reformatting_data(data: list) -> list:
         sensor[key] = temp
 
     # Taking rows and packing the data in blocks
-
-    lista_completa = []
-    for i in range(lists_sizes[0]):
+    package_data = []
+    for size_value in range(lists_sizes[0]):
         temp2 = {}
         dict_bloque = {}
-        for var in sensor:
-            temp = sensor[var]
-            temp2[var] = temp[i]
+        for block in sensor:
+            temp = sensor[block]
+            temp2[block] = temp[size_value]
         dict_bloque["equipo_ip"] = "172.21.14.13"
-        dict_bloque["fecha_hora"] = fechas[i]
+        dict_bloque["fecha_hora"] = fechas[size_value]
         dict_bloque["sensores"] = temp2
-        lista_completa.append(dict_bloque)
+        package_data.append(dict_bloque)
 
-    return lista_completa
-
-
-def query_select() -> list:
-    # database connection
-    connection = pymysql.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, database=mysql_db)
-    cursor = connection.cursor()
-
-    retrieve = f"SELECT * FROM {mysql_table1} WHERE ({column_d2} BETWEEN '{fecha_hora1}' AND '{fecha_hora2}') AND {column_d1}=0"
-    cursor.execute(retrieve)
-    result = cursor.fetchall()
-
-    # committing the connection then closing it.
-    connection.commit()
-    connection.close()
-    return result
+    return package_data
 
 
 if __name__ == '__main__':
-    outdated_data = query_select()
-    outdated_list = [list(elem) for elem in outdated_data]
-    formatted_data = reformatting_data(outdated_list)
+    formatted_data = query_select()
 
     with open('formatted_data.json', 'w', encoding='utf-8') as f:
         json.dump(formatted_data, f, ensure_ascii=False, indent=4, default=str)
@@ -110,6 +99,8 @@ if __name__ == '__main__':
     # SFTP connection
     files = glob.glob(folder_path + file_type)
     max_file = max(files, key=os.path.getctime)
+    folder_path = "{0}\\".format(folder_path)
+    file = max_file.replace(folder_path, '')
 
     with pysftp.Connection(myHostname, username=myUsername, password=myPassword) as sftp:
         localFilePath = max_file
